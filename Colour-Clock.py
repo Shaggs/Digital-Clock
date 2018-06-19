9b71d19fc8f23d15c6e9588c488bf97a6c7210cc
Shane Rees <rees.shane@gmail.com>
2018-06-19 12:22:05 +0930

    Added foreground Colours

--- Colour-Clock.py
+++ Colour-Clock.py
@@ -1,92 +1,48 @@
-# use Tkinter to show a digital clock
+# A small program for learning TK
 
-# tested with Python24 vegaseat 10sep2006
-
-
+# Imports
 from tkinter import *
 from datetime import datetime, time
 from pytz import timezone
 from ConfigParser import SafeConfigParser
+# Load in settings
 parser = SafeConfigParser()
 parser.read('settings.conf')
 style = str(parser.get('settings', 'Font'))#Set
 fsize = int(parser.get('settings', 'Size'))#Set
-HourColour = str(parser.get('settings', 'Hour-Colour'))#Set
-QuarterColour = str(parser.get('settings', 'Quarter-Colour'))#Set
-HalfColour = str(parser.get('settings', 'Half-Colour'))#Set
-ThreeQuaterColour = str(parser.get('settings', 'ThreeQuater-Colour'))#Set
+HourColour = str(parser.get('settings', 'BG-Hour-Colour'))#Set
+QuarterColour = str(parser.get('settings', 'BG-Quarter-Colour'))#Set
+HalfColour = str(parser.get('settings', 'BG-Half-Colour'))#Set
+ThreeQuaterColour = str(parser.get('settings', 'BG-ThreeQuater-Colour'))#Set
+FGHour  = str(parser.get('settings', 'Text-Hour-Colour'))#Set
+FGQuater  = str(parser.get('settings', 'Text-Quater-Colour'))#Set
+FGHalf  = str(parser.get('settings', 'Text-Half-Colour'))#Set
+GFThreeQuater  = str(parser.get('settings', 'Text-ThreeQuater-Colour'))#Set
 textcolour = str(parser.get('settings', 'Text-Colour'))#Set
 fheight = int(parser.get('settings', 'Frame-Height'))#Set
 fwidth = int(parser.get('settings', 'Frame-Width'))#Set
-
-
-
-
+# Start TK
 root = Tk()
-
+clock = Label(root, font=(style, fsize, 'bold'), bg="green")
+clock.pack(fill=BOTH, expand=0)
 format=("%d/%m/%y  %H:%M:%S")
-f=("%M")
-
-now = datetime.now(timezone('Australia/Adelaide'))
-time = str(now.strftime(format))
-swap = str(now.strftime(f))
-
-def ThreeQuater():
+Minute=("%M")
+# Define main program
+def main():
 	now = datetime.now(timezone('Australia/Adelaide'))
 	time = str(now.strftime(format))
-	swap = str(now.strftime(f))
-	clock.config(text=time, height=fheight, width=fwidth, bg=ThreeQuaterColour)
-	clock.after(200, ThreeQuater)
-	if swap == "00":
-		clock.after(200,Hour)
-
-def Half():
-	now = datetime.now(timezone('Australia/Adelaide'))
-	time = str(now.strftime(format))
-	swap = str(now.strftime(f))
-	clock.config(text=time, height=fheight, width=fwidth, bg=HalfColour)
-	clock.after(200, Half)
-	if swap == "45":
-		clock.after(200,ThreeQuater)
-
-def Quarter():
-	now = datetime.now(timezone('Australia/Adelaide'))
-	time = str(now.strftime(format))
-	swap = str(now.strftime(f))
-
-	clock.config(text=time, height=fheight, width=fwidth, bg=QuarterColour)
-	clock.after(200, Quarter)
+	swap = str(now.strftime(Minute))
+	clock.config(text=time, height=fheight, width=fwidth)
+	clock.after(200, main)
 	if swap == "30":
-		clock.after(200,Half)
-
-
-def Hour():
-	now = datetime.now(timezone('Australia/Adelaide'))
-	time = str(now.strftime(format))
-	swap = str(now.strftime(f))
-	clock.config(text=time, height=fheight, width=fwidth,bg=HourColour)
-	clock.after(200, Hour)
+		clock.config(bg=HalfColour, foreground=FGHalf)
+	if swap == "45":
+		clock.config(bg=ThreeQuaterColour, foreground=FGThreeQuater)
+	if swap == "00":
+		clock.config(bg=HourColour, foreground=FGHour)
 	if swap == "15":
-		clock.after(200,Quarter)
-
-
-print swap
-if swap > "15" and swap < "30":
-	clock = Label(root, font=(style, fsize, 'bold'))
-	clock.pack(fill=BOTH, expand=0)
-	Quarter()
-elif swap > "30" and swap < "45":
-	clock = Label(root, font=(style, fsize, 'bold'))
-	clock.pack(fill=BOTH, expand=0)
-	Half()
-elif swap > "45"and swap < "59":
-	clock = Label(root, font=(style, fsize, 'bold'))
-	clock.pack(fill=BOTH, expand=0)
-	ThreeQuarter()
-elif swap > "00"and swap < "15":
-	clock = Label(root, font=(style, fsize, 'bold'))
-	clock.pack(fill=BOTH, expand=0)
-	Hour()
-
-root.mainloop( )
-
+		clock.config(bg=QuarterColour, foreground=FGQuater)
+# Run Main Program
+if __name__=="__main__":
+	main()
+	root.mainloop()
